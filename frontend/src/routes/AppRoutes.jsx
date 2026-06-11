1287370a74b4e004edd4d3fbf5c7be47bfc5af52
pedroMarcelino <pedro.marcelinodv@gmail.com>
2026-06-11 15:10:18 -0300

    cricacao de tela de login e concexao com axios

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home/Home";
 import Login from "../pages/Auth/Login";
+import Register from "../pages/Auth/Register";
 
 function AppRoutes() {
     return (
@@ -9,6 +10,7 @@ function AppRoutes() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </BrowserRouter>
     );
